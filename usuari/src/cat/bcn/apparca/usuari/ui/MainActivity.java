53f729f074aa1d57e2feb29fa4f1812ab4b015da
sergi.martinez@gmail.com <sergi.martinez@gmail.com@011025a0-9c83-6cad-29e5-49d328e92733>
2011-12-15 09:43:38 +0000

    Initial import for usuari

---
+++ usuari/src/cat/bcn/apparca/usuari/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package cat.bcn.apparca.usuari.ui;
+
+import cat.bcn.apparca.usuari.R;
+import cat.bcn.apparca.usuari.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
