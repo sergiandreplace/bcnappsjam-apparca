b96841cb039f112b261a12550a205337aefcfe5a
sergi.bc@gmail.com <sergi.bc@gmail.com@011025a0-9c83-6cad-29e5-49d328e92733>
2011-12-15 12:58:06 +0000

    first commit

---
+++ trunk/usuari/src/cat/bcn/apparca/usuari/ui/utils/Constants.java
@@ -0,0 +1,21 @@
+package cat.bcn.apparca.usuari.ui.utils;
+
+public class Constants {
+	
+	public static final String TAG = "APPARCA";
+
+	public static final String INTENT_MAIN = "android.intent.action.MAIN";
+	public static final String INTENT_MAIN_ACTIVITY = "cat.bcn.apparca.agent.ui.MainTabActivity";
+	public static final String INTENT_GEOPOSITION = "cat.bcn.apparca.agent.ui.GeopositionActivity";
+	public static final String INTENT_PAYMENT = "cat.bcn.apparca.agent.ui.PaymentActivity";
+	public static final String INTENT_TRACKING = "cat.bcn.apparca.agent.ui.TrackingActivity";
+	public static final String INTENT_PROFILE = "cat.bcn.apparca.agent.ui.ProfileActivity";
+	public static final String INTENT_INFO = "cat.bcn.apparca.agent.ui.InfoActivity";
+
+//	public static final int TAB_MAIN = 0;
+	public static final int TAB_PROCESS = 0;
+	public static final int TAB_PROFILE = 1;
+	public static final int TAB_INFO = 2;
+
+	public static final String EXTRA_TAB_POSITION = "tab_position";
+}
